4cdd767973d49468cf41d2470918c5fcb64076db
siva <sivaroyal423@gmail.com>
2021-03-17 00:50:58 +0530

    shiva

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React  from 'react'
+import Header from './Components/Header/Header'
+
+import {  } from './Styles.css';
+function App() {
+ 
+  return (
+    <div className="App.jsx">
+      
+      <Header/>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Styles.css'
+ class Header extends React.Component {
+    render () {
+    return(
+        <div className="header">
+            <h1>SPACE-X</h1>
+        </div>
+    )
+}
+}
+export default Header
